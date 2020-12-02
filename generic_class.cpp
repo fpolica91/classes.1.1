3a203a9d2f9e43bdf94124a5a1ec296d031ead3e
Fabricio <fabriciopolicarpo0@gmail.com>
2020-12-02 06:05:45 +0000

    generic

--- generic_class.cpp
+++ generic_class.cpp
@@ -6,32 +6,32 @@ using namespace std;
 // t is the generic variable 
 // t is a generic data type -> t can be int, char, string;
 template <class T>
-class Arithmetic{
+class Generic{
   private:
     T a;
     T b;
 
   public:
-    Arithmetic(T a, T b);
+    Generic(T a, T b);
     T add();
     T sub();
 };
 
 template<class T>
-  Arithmetic<T>::Arithmetic(T a, T b){
+  Generic<T>::Generic(T a, T b){
   this -> a =a;
   this -> b =b;
 };
 
 template <class T>
- T Arithmetic<T>::add(){
+ T Generic<T>::add(){
    T c;
    c = a + b;
    return c;
  }
 
  template <class T>
- T Arithmetic<T>::sub(){
+ T Generic<T>::sub(){
    T c;
    c = a - b;
    return c;
